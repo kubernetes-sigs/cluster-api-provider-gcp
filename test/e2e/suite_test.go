814c559b5736bebeca6c24908b1ea5fb838145ee
sayantani11 <ii.sayantani.ii@gmail.com>
2022-08-30 02:37:18 +0530

    Tests and Yamls

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -209,6 +209,7 @@ func setupBootstrapCluster(config *clusterctl.E2EConfig, scheme *runtime.Scheme,
 			Name:               config.ManagementClusterName,
 			RequiresDockerSock: config.HasDockerProvider(),
 			Images:             config.Images,
+			IPFamily:           config.GetVariable(IPFamily),
 		})
 		Expect(clusterProvider).ToNot(BeNil(), "Failed to create a bootstrap cluster")
 
