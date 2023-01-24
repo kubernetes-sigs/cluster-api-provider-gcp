396e2810c7b8ba8ba82cbf47a898ce47e61bd970
Richard Chen <richardchen331@gmail.com>
2023-01-24 15:42:36 -0800

    make generate

--- api/v1beta1/zz_generated.deepcopy.go
+++ api/v1beta1/zz_generated.deepcopy.go
@@ -22,7 +22,7 @@ limitations under the License.
 package v1beta1
 
 import (
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	apiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/cluster-api/errors"
