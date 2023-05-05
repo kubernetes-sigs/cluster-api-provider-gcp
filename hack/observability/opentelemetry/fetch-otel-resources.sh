bce50973856c39853d2cfbcf75c6b572fc71e522
phong-nguyen <phongnguyen01.work@gmail.com>
2023-05-05 02:10:35 +0000

    helm charts for jaeger all-in-one + otel collector

---
+++ hack/observability/opentelemetry/fetch-otel-resources.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# "Borrow" with permission from CAPZ team: https://github.com/kubernetes-sigs/cluster-api-provider-azure/blob/main/hack/observability/opentelemetry/fetch-otel-resources.sh
+
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+CHART_RELEASE=${CHART_RELEASE:-0.53.0}
+OTEL_ROOT=$(dirname "${BASH_SOURCE[0]}")
+CHART_ROOT=$OTEL_ROOT/chart
+
+
+rm -rf "$CHART_ROOT"
+# # "tar" has no POSIX standard, so use only basic options and test with both BSD and GNU.
+wget -qO- https://github.com/open-telemetry/opentelemetry-helm-charts/releases/download/opentelemetry-collector-"$CHART_RELEASE"/opentelemetry-collector-"$CHART_RELEASE".tgz \
+  | tar xvz -C "$OTEL_ROOT" --exclude "ci" --exclude "examples"
+mv "$OTEL_ROOT"/opentelemetry-collector "$CHART_ROOT"
+wget -q https://raw.githubusercontent.com/open-telemetry/opentelemetry-helm-charts/main/LICENSE -P "$CHART_ROOT"
